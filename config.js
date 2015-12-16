25eea9aa1b6575c0c9916e067d14bb4a5ddd696f
BDLazar <biancadenise.lazar@mydit.ie>
2015-12-16 21:04:56 +0000

    mongodb

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
-  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost/tictactoe'
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://appadmin:password123@ds033145.mongolab.com:33145/heroku_8lrlb318'
 };
